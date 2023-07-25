ba89f8bade46484668a342a1992c07897cb81901
Ahmet Rüchan Dora <ahmetdoraa@gmail.com>
2023-07-26 01:23:24 +0300

    CodeFirstProject

---
+++ CodeFirstProject/Context/MyProjectDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstProject.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstProject.Context
+{
+    public class MyProjectDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=MyProject;User Id=rchn;Password=1234;Encrypt=False;TrustServerCertificate=False;");
+        }
+    }
+    public class Start
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Project2/Class2.cs
@@ -0,0 +1,39 @@
+﻿using CodeFirstProject.Context;
+using CodeFirstProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    public class Class2:Interface2
+    {
+        MyProjectDbContext context = new();
+        public Class2(MyProjectDbContext context)
+        {
+            this.context = context;
+        }
+        public void CreateUser()
+        {
+            User user = new()
+            {
+                Name = "Ahmet",
+                LastName = "Dora",
+            };
+
+            context.Users.Add(user); 
+            context.SaveChanges();
+        }
+    }
+    class Start
+    {
+        public static void Main(string[] args)
+        {
+            MyProjectDbContext context = new();
+            Interface2 class2Instance = new Class2(context);
+            class2Instance.CreateUser();
+        }
+    }
+}
